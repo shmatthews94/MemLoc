929e2c65164e8279425dc2ba5315f05eb9f22171
Shane Matthews <shmatthews94@gmail.com>
2015-09-16 01:52:54 -0400

    radio buttons, reminders added

--- app/src/main/java/meets/chane/memloc/LocMem.java
+++ app/src/main/java/meets/chane/memloc/LocMem.java
@@ -6,17 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.EditText;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class LocMem extends AppCompatActivity {
 
     private Button Add;
-    private CheckBox Leaving;
-    private CheckBox Arriving;
-    private EditText Added;
+    private RadioButton Leaving;
+    private RadioButton Arriving;
+    private TextView Added;
     private EditText Task;
     private EditText Location;
     private ArrayList<Reminder> Reminders;
@@ -27,11 +29,12 @@ public class LocMem extends AppCompatActivity {
         setContentView(R.layout.activity_loc_mem);
 
         Add = (Button) findViewById(R.id.Add);
-        Leaving = (CheckBox) findViewById(R.id.Leaving);
-        Arriving = (CheckBox) findViewById(R.id.Arriving);
-        Added = (EditText) findViewById(R.id.Added);
+        Leaving = (RadioButton) findViewById(R.id.Leaving);
+        Arriving = (RadioButton) findViewById(R.id.Arriving);
+        Added = (TextView) findViewById(R.id.Added);
         Task = (EditText) findViewById(R.id.Task);
         Location = (EditText) findViewById(R.id.Location);
+        Reminders = new ArrayList<Reminder>();
     }
 
     @Override
@@ -57,14 +60,28 @@ public class LocMem extends AppCompatActivity {
     }
 
     public void createReminder(View v) {
-        /*
         Reminder q = new Reminder(true, "home");
         //Reminder r = new Reminder(this.Leaving.isChecked(), this.Location.getText().toString());
         Reminders.add(q);
         if(Reminders.contains(q)) {
             this.Added.setText("Added!");
         }
-         */
-        this.Added.setText("Added!");
+    }
+
+    public void onRadioButtonClicked(View v) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) v).isChecked();
+
+        // Check which radio button was clicked
+        switch(v.getId()) {
+            case R.id.Leaving:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.Arriving:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
     }
 }
