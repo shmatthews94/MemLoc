284339d5727c9ec9fb980598c95fb294e3ec1787
Shane Matthews <shmatthews94@gmail.com>
2015-09-16 01:03:14 -0400

    add button print statement, checkboxes added

--- app/src/main/java/meets/chane/memloc/LocMem.java
+++ app/src/main/java/meets/chane/memloc/LocMem.java
@@ -4,13 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class LocMem extends AppCompatActivity {
+
+    private Button Add;
+    private CheckBox Leaving;
+    private CheckBox Arriving;
+    private EditText Added;
+    private EditText Task;
+    private EditText Location;
+    private ArrayList<Reminder> Reminders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loc_mem);
+
+        Add = (Button) findViewById(R.id.Add);
+        Leaving = (CheckBox) findViewById(R.id.Leaving);
+        Arriving = (CheckBox) findViewById(R.id.Arriving);
+        Added = (EditText) findViewById(R.id.Added);
+        Task = (EditText) findViewById(R.id.Task);
+        Location = (EditText) findViewById(R.id.Location);
     }
 
     @Override
@@ -33,5 +54,17 @@ public class LocMem extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createReminder(View v) {
+        /*
+        Reminder q = new Reminder(true, "home");
+        //Reminder r = new Reminder(this.Leaving.isChecked(), this.Location.getText().toString());
+        Reminders.add(q);
+        if(Reminders.contains(q)) {
+            this.Added.setText("Added!");
+        }
+         */
+        this.Added.setText("Added!");
     }
 }
---
+++ app/src/main/java/meets/chane/memloc/Reminder.java
@@ -0,0 +1,20 @@
+package meets.chane.memloc;
+import java.sql.Timestamp;
+import java.util.Date;
+import android.location.Location;
+
+/**
+ * Created by shanematthews on 9/15/15.
+ */
+public class Reminder {
+    private Date created;
+    private boolean leaving;
+    private String location;
+
+    public Reminder(boolean leave, String location) {
+        this.location = location;
+        this.leaving = leave;
+        Date date = new java.util.Date();
+        this.created = date;
+    }
+}
