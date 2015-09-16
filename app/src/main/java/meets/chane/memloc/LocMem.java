a58f974a9ac6ded4feb51a78e6d288aa0b15e472
Chase Deets <chasedeets@Chases-MacBook-Air.local>
2015-09-16 17:37:34 -0400

    added google play api

--- app/src/main/java/meets/chane/memloc/LocMem.java
+++ app/src/main/java/meets/chane/memloc/LocMem.java
@@ -1,10 +1,11 @@
 package meets.chane.memloc;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
+//import android.Manifest;
+//import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.location.Location;
@@ -14,13 +15,19 @@ import android.widget.EditText;
 import android.view.View;
 import android.widget.TextView;
 import android.content.Context;
+//import android.location.Location;
+//import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.meets.chane.memloc.R;
 
 import java.util.ArrayList;
 
-public class LocMem extends AppCompatActivity {
+public class LocMem extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener{
 
     private Button Add;
     private RadioButton Leaving;
@@ -33,7 +40,10 @@ public class LocMem extends AppCompatActivity {
     protected Location mLastLocation;
     protected TextView mLatitudeText;
     protected TextView mLongitudeText;
+    protected static final String TAG = "memloc sample";
     LocationManager manager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +60,19 @@ public class LocMem extends AppCompatActivity {
         mLatitudeText = (TextView) findViewById((R.id.mLatitudeText));
         mLongitudeText = (TextView) findViewById((R.id.mLongitudeText));
         manager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        //buildGoogleApiClient();
+        buildGoogleApiClient();
+
     }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -101,6 +122,37 @@ public class LocMem extends AppCompatActivity {
                     // Ninjas rule
                     break;
         }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // Provides a simple way of getting a device's location and is well suited for
+        // applications that do not require a fine-grained location and that do not need location
+        // updates. Gets the best and most recent location currently available, which may be null
+        // in rare cases when a location is not available.
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        } else {
+            Log.i(TAG, "no location detected");
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
     }
 
     /*
